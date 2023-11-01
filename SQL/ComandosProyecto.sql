25d05d455004501277f16bd5c925441fc6e151bb
Matias Lovato <matii_seba_11@hotmail.com>
2023-11-01 11:34:20 -0300

    (Squashed commit) 1ra Entrega
    commit 3dcbb1b6269ea18942da9122633b1e42b0ed2519
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Tue Oct 31 00:26:23 2023 -0300
    
        Documentacion agregada
    
    commit 392cc32a6ee2fb83ac21ed6d9b58ab4e86667c4b
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Mon Oct 30 22:17:36 2023 -0300
    
        combinacion de lo hecho con los compañeros

---
+++ SQL/ComandosProyecto.sql
@@ -0,0 +1,47 @@
+USE base_consorcioPI;
+
+-- se necesita tener una instancia MIXTA para realizar esto
+-- Crear dos usuarios de base de datos:
+CREATE LOGIN UsuarioAdmin WITH PASSWORD = 'pwAdmin';
+CREATE LOGIN UsuarioSoloLectura WITH PASSWORD = 'pwSoloLectura';
+
+
+
+--Asignar permisos al usuario de administrador:
+CREATE USER UsuarioAdmin FOR LOGIN UsuarioAdmin;
+ALTER ROLE db_owner ADD MEMBER UsuarioAdmin;
+
+--Asignar permisos al usuario de solo lectura:
+CREATE USER UsuarioSoloLectura FOR LOGIN UsuarioSoloLectura;
+GRANT EXECUTE TO UsuarioSoloLectura;
+
+
+--damos acceso al usuario de solo lectura al procedimiento
+GRANT EXECUTE ON dbo.procedimiento_insert_administrador TO UsuarioSoloLectura;
+
+
+-- Inserción con el usuario de administrador
+EXECUTE AS LOGIN = 'UsuarioAdmin'; --Execute as login se usa para ejecutar usando permisos especiales
+INSERT INTO administrador (apeynom, viveahi, tel, sexo, fechnac) VALUES ('Admin', 'S', '123456', 'M', GETDATE());
+REVERT; --REVERT en SQL Server se utiliza para volver al contexto de seguridad original
+
+
+-- Inserción con el usuario de solo lectura a través del procedimiento almacenado
+EXECUTE AS LOGIN = 'UsuarioSoloLectura';
+INSERT INTO administrador (apeynom, viveahi, tel, sexo, fechnac) VALUES ('Admin', 'S', '123456', 'M', GETDATE()); -- NO PODRA HACERLO 
+REVERT;
+
+
+
+--insercion con procedimiento
+EXECUTE AS LOGIN = 'UsuarioAdmin'; 
+EXEC procedimiento_insert_administrador;
+REVERT; 
+
+--insercion con procedimiento
+EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
+EXEC procedimiento_insert_administrador; --si podra ejecutarlo porque tiene permiso de ejecucion
+REVERT; 
+
+
+
