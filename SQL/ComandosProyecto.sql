e3f4006b97f0ddd749bd7cc56ae15977042ed3c7
MatiasMarin9 <marinmatias992@gmail.com>
2023-11-18 23:51:52 -0300

    Aplicación de TRIGGERS y corrección

--- SQL/ComandosProyecto.sql
+++ SQL/ComandosProyecto.sql
@@ -1,6 +1,6 @@
 USE base_consorcioPI;
 
--- se necesita tener una instancia MIXTA para realizar esto
+-- Se necesita tener una instancia MIXTA para realizar esto
 -- Crear dos usuarios de servidor SQL Server:
 CREATE LOGIN UsuarioAdmin WITH PASSWORD = 'pwAdmin';
 CREATE LOGIN UsuarioSoloLectura WITH PASSWORD = 'pwSoloLectura';
@@ -10,16 +10,13 @@ CREATE USER UsuarioAdmin FOR LOGIN UsuarioAdmin;
 --Asignar permisos al usuario de administrador:
 ALTER ROLE db_owner ADD MEMBER UsuarioAdmin;
 
-
 --Crea usuario dentro de la base consorcio
 CREATE USER UsuarioSoloLectura FOR LOGIN UsuarioSoloLectura;
 --Asignar permisos al usuario de solo lectura:
 ALTER ROLE db_datareader ADD MEMBER UsuarioSoloLectura;
 
---GRANT EXECUTE TO UsuarioSoloLectura;
-
-
---damos acceso al usuario de solo lectura al procedimiento
+--Creado previamente el procedimiento almacenado
+--Damos acceso al usuario de solo lectura al procedimiento
 GRANT EXECUTE ON dbo.InsertarAdministrador TO UsuarioSoloLectura;
 
 
@@ -35,28 +32,42 @@ INSERT INTO administrador (apeynom, viveahi, tel, sexo, fechnac) VALUES ('Admin'
 REVERT;
 
 
-
-
-
---insercion con procedimiento Admin
+--Insercion con procedimiento Admin
 EXECUTE AS LOGIN = 'UsuarioAdmin'; 
 EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828';
 REVERT; 
-
-
-
 
 --insercion con procedimiento Solo lectura
 EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
 EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828'; --si podra ejecutarlo
 REVERT; 
 
-
 -- Revocar permiso de ejecución del procedimiento almacenado
-REVOKE EXECUTE ON dbo.procedimiento_insert_administrador FROM UsuarioSoloLectura; 
+REVOKE EXECUTE ON dbo.InsertarAdministrador FROM UsuarioSoloLectura; 
 
-
---insercion con procedimiento con el permiso revocado
+--Insercion con procedimiento con el permiso revocado
 EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
 EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828'; --ya no podra ejecutarlo porque le quitamos el permiso
 REVERT; 
+
+--Creamos la tabla AUDITORIA para la utilización de TRIGGER
+
+--Creamos TRIGGER para administrador
+
+--Probamos TRIGGER 
+
+	--Insertamos registros con el administrador
+	EXECUTE AS LOGIN = 'UsuarioAdmin'; 
+	EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828';
+	REVERT; 
+
+	--Visualizamos resultados
+	Select * from auditoria
+
+	--Insertamos registros con el usuario de sólo lectura
+	EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
+	EXEC InsertarAdministrador 'RONCAGLIA ALFREDO ALEJANDRO', 'S', 3794244222, 'M', '19220828'; --si podra ejecutarlo
+	REVERT; 
+
+	--Visualizamos resultados
+	Select * from auditoria
