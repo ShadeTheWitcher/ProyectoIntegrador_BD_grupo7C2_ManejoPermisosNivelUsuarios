fb20bbd97f5b48ff9bfaba6cafc0c2e39b4d8763
Matias Lovato <matii_seba_11@hotmail.com>
2023-11-20 14:34:23 -0300

    (Squashed) Update Documentacion
    commit 008052076966f9f11d08a78b2a8e8ac23264cf3b
    Author: MatiasMarin9 <marinmatias992@gmail.com>
    Date:   Mon Nov 20 13:38:24 2023 -0300
    
        Presentación Final del Proyecto
    
    commit 2fec657e896bfc5995a749d71c7f1730574a51e3
    Merge: 0e99ceb e3f4006
    Author: MatiasMarin9 <marinmatias992@gmail.com>
    Date:   Sat Nov 18 23:57:35 2023 -0300
    
        Merge branch 'Marín_Matias' into Desarrollo
    
    commit e3f4006b97f0ddd749bd7cc56ae15977042ed3c7
    Author: MatiasMarin9 <marinmatias992@gmail.com>
    Date:   Sat Nov 18 23:51:52 2023 -0300
    
        Aplicación de TRIGGERS y corrección
    
    commit 0e99cebe244a26ead5a26af139c2bdc23b277f79
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Sat Nov 18 23:44:37 2023 -0300
    
        (Vistas) manejo de permisos con vistas
    
    commit 47bcac870b2ac7dfb7ec6ae2b071a374905692b8
    Merge: 94547db 161cd00
    Author: MatiasMarin9 <marinmatias992@gmail.com>
    Date:   Sat Nov 18 22:56:52 2023 -0300
    
        Merge remote-tracking branch 'origin/Desarrollo' into Marín_Matias
    
    commit 161cd003f9c5a1cdf444f040607c0b23246af5fd
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Fri Nov 17 23:44:12 2023 -0300
    
        Update README.md
    
    commit 14ee13b7d71ba4de013c989c8511edc34e939173
    Author: Matias Lovato <85040178+ShadeTheWitcher@users.noreply.github.com>
    Date:   Fri Nov 17 23:41:35 2023 -0300
    
        Update README.md
    
    commit ba4ed1fee557415e4a59f1189fdba6822a945ee9
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Fri Nov 17 23:37:44 2023 -0300
    
        Create README.md
    
    commit bbbdb5f45d67af7eb7203efc8e7589c9bd833ec3
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Fri Nov 17 23:07:17 2023 -0300
    
        adaptacion y implementacion del codigo de PA
    
        procedimientos almacenados
    
    commit 4f7cc995bffea72067c671f09195cd6a6c320dec
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Mon Nov 13 21:30:41 2023 -0300
    
        uso de Revoke
    
    commit 42a691dc334bdd3a47b48eaebb283e34d16fb523
    Merge: 467d337 649ecb5
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Mon Nov 13 21:29:00 2023 -0300
    
        Merge branch 'Desarrollo' into Lovato_Matias
    
    commit 649ecb51e2adf9d3dc2dbda5ae95f4211be22e49
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Mon Nov 13 21:28:24 2023 -0300
    
        cambio de nombre archivo
    
    commit 3dcbb1b6269ea18942da9122633b1e42b0ed2519
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Tue Oct 31 00:26:23 2023 -0300
    
        Documentacion agregada
    
    commit 392cc32a6ee2fb83ac21ed6d9b58ab4e86667c4b
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Mon Oct 30 22:17:36 2023 -0300
    
        combinacion de lo hecho con los compañeros
    
    commit 467d3373461b5d21ee1177380df2ba0dc4161ab6
    Author: Matias Lovato <matii_seba_11@hotmail.com>
    Date:   Thu Oct 26 20:57:44 2023 -0300
    
        Update ProyectoIntegradorC2_grupo7.sql
    
    commit 94547db96d2fb27af519bbf97bc2b2ad939e5f7a
    Author: MatiasMarin9 <89382918+MatiasMarin9@users.noreply.github.com>
    Date:   Thu Oct 26 20:57:20 2023 -0300
    
        Primera Versión Proyecto (Práctico)
    
    Co-Authored-By: MatiasMarin9 <89382918+MatiasMarin9@users.noreply.github.com>
    Co-Authored-By: Maximiliano Daniel Gimenez <77470572+Daxigi@users.noreply.github.com>
    Co-Authored-By: Octavio Gauna <80434721+VictorOctavio@users.noreply.github.com>

--- SQL/ComandosProyecto.sql
+++ SQL/ComandosProyecto.sql
@@ -1,6 +1,6 @@
 USE base_consorcioPI;
 
--- se necesita tener una instancia MIXTA para realizar esto
+-- Se necesita tener una instancia MIXTA para realizar esto
 -- Crear dos usuarios de servidor SQL Server:
 CREATE LOGIN UsuarioAdmin WITH PASSWORD = 'pwAdmin';
 CREATE LOGIN UsuarioSoloLectura WITH PASSWORD = 'pwSoloLectura';
@@ -10,16 +10,13 @@ CREATE USER UsuarioAdmin FOR LOGIN UsuarioAdmin;
 --Asignar permisos al usuario de administrador:
 ALTER ROLE db_owner ADD MEMBER UsuarioAdmin;
 
-
 --Crea usuario dentro de la base consorcio
 CREATE USER UsuarioSoloLectura FOR LOGIN UsuarioSoloLectura;
 --Asignar permisos al usuario de solo lectura:
 ALTER ROLE db_datareader ADD MEMBER UsuarioSoloLectura;
 
---GRANT EXECUTE TO UsuarioSoloLectura;
-
-
---damos acceso al usuario de solo lectura al procedimiento
+--Creado previamente el procedimiento almacenado
+--Damos acceso al usuario de solo lectura al procedimiento
 GRANT EXECUTE ON dbo.InsertarAdministrador TO UsuarioSoloLectura;
 
 
@@ -35,28 +32,87 @@ INSERT INTO administrador (apeynom, viveahi, tel, sexo, fechnac) VALUES ('Admin'
 REVERT;
 
 
-
-
-
---insercion con procedimiento Admin
+--Insercion con procedimiento Admin
 EXECUTE AS LOGIN = 'UsuarioAdmin'; 
 EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828';
 REVERT; 
-
-
-
 
 --insercion con procedimiento Solo lectura
 EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
 EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828'; --si podra ejecutarlo
 REVERT; 
 
-
 -- Revocar permiso de ejecución del procedimiento almacenado
-REVOKE EXECUTE ON dbo.procedimiento_insert_administrador FROM UsuarioSoloLectura; 
+REVOKE EXECUTE ON dbo.InsertarAdministrador FROM UsuarioSoloLectura; 
 
-
---insercion con procedimiento con el permiso revocado
+--Insercion con procedimiento con el permiso revocado
 EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
 EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828'; --ya no podra ejecutarlo porque le quitamos el permiso
 REVERT; 
+
+--Creamos la tabla AUDITORIA para la utilización de TRIGGER
+
+--Creamos TRIGGER para administrador
+
+--Probamos TRIGGER 
+
+	--Insertamos registros con el administrador
+	EXECUTE AS LOGIN = 'UsuarioAdmin'; 
+	EXEC InsertarAdministrador 'LOPEZ JUAN CARLOS', 'S', 3794222222, 'M', '19920828';
+	REVERT; 
+
+	--Visualizamos resultados
+	Select * from auditoria
+
+	--Insertamos registros con el usuario de sólo lectura
+	EXECUTE AS LOGIN = 'UsuarioSoloLectura'; 
+	EXEC InsertarAdministrador 'RONCAGLIA ALFREDO ALEJANDRO', 'S', 3794244222, 'M', '19220828'; --si podra ejecutarlo
+	REVERT; 
+
+	--Visualizamos resultados
+	Select * from auditoria
+
+
+
+--///////////////////   EJEMPLO CON VISTAS  ////////////////////////---
+
+--Creamos otro usuario para probar
+CREATE LOGIN UsuarioVista WITH PASSWORD = 'pwVista';
+CREATE USER UsuarioVista FOR LOGIN UsuarioVista;
+
+
+
+--Otrogamos permiso de select en la tabla admin
+GRANT SELECT ON dbo.administrador TO UsuarioVista;
+
+EXECUTE AS LOGIN = 'UsuarioVista'; 
+SELECT * from administrador --comprobamos que puede usar select en administrador
+REVERT;
+
+
+
+
+--Revocamos los permisos directos de SELECT en la tabla administrador para evitar que el usuario acceda directamente a ella.
+REVOKE SELECT ON dbo.administrador FROM UsuarioVista;
+
+EXECUTE AS LOGIN = 'UsuarioVista'; 
+SELECT * from administrador --comprobamos que no puede usar select en administrador
+REVERT;
+
+
+
+
+-- Creamos una vista que permitira que solo se visualize las columnas especificadas
+CREATE VIEW dbo.VistaAdministrador AS
+SELECT idadmin, apeynom, tel, sexo, fechnac
+FROM dbo.administrador;
+
+--Otorgamos permisos SELECT al usuario en la vista creada, permitiéndole consultar datos a través de esta vista.
+GRANT SELECT ON dbo.VistaAdministrador TO UsuarioVista;
+
+--Probamos la vista
+EXECUTE AS LOGIN = 'UsuarioVista'; 
+SELECT * FROM dbo.VistaAdministrador; --aparecera la vista que creamos antes
+REVERT;
+
+
